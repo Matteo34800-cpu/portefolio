ce21cc8c3287b9cea1c0b9e65cf8dd4813425674
melandrim <matteo.melandri-dalloyau@etu.umontpellier.fr>
2025-06-15 22:05:23 +0200

    Projets component avancé

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { AccueilComponent } from './Component/acceuil/acceuil.component';
 import { AppComponent } from './app.component';
 import { IntroComponent } from './Component/intro/intro.component';
 import { animation } from '@angular/animations';
+import { ProjetsComponent } from './Component/projets/projets.component';
 
 export const routes: Routes = [
     {path: '', component: IntroComponent, title: 'Main', data:{animation: 'introPage'}},
     {path: 'intro', component: IntroComponent, title: 'Intro', data:{animation: 'introPage'}},
     {path: 'accueil', component: AccueilComponent, title: 'Accueil', data:{animation: 'accueilPage'}},
+    {path: 'projets', component: ProjetsComponent, title: 'Projets', data:{animation: 'projetsPage'}},
 ];
