d7980f0571936f8b711f96e0d3a72df46c8590b7
melandrim <matteo.melandri-dalloyau@etu.umontpellier.fr>
2025-12-19 11:08:21 +0100

    modif avancée

--- frontAngular/src/app/Component/acceuil/acceuil.component.ts
+++ frontAngular/src/app/Component/acceuil/acceuil.component.ts
@@ -1,12 +1,13 @@
 import { Component, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { CategoryService } from '../../category.service'; // Adjust the path as necessary
 
 
 @Component({
   selector: 'app-accueil',
   templateUrl: './acceuil.component.html',
-  imports: [CommonModule],
+  imports: [CommonModule,RouterModule],
   styleUrls: ['./acceuil.component.css'],
   standalone: true
 })
@@ -48,7 +49,4 @@ export class AccueilComponent {
     this.CategoryChange.emit(snapTo === 0 ? 'info' : 'escalade');
   }
 
-
-bioInfo = "Bonjour, je m'appelle Matteo Melandri, j’ai 19 ans et je suis actuellement en BUT 2 Informatique. Durant mon alternance, j’ai eu l’occasion de travailler sur divers outils et technologies tels que Angular et Git, et de développer mes compétences en gestion de projets en tant que SCRUM Master d’une équipe de développement web. \n Passionné par le développement et les nouvelles technologies, je m’investis pleinement dans chaque projet et aime relever de nouveaux défis. Mon objectif est de continuer à apprendre et à acquérir de l’expérience, tout en contribuant à des projets innovants et structurants."
-
-bioEscalade = 'Je pratique l’escalade depuis 8 ans. Ce sport est pour moi une véritable source de liberté et de dépassement de soi. Je suis professeur d’escalade bénévole dans une association, où j’encadre un groupe d’adolescents chaque mardi. Ce que j’aime par-dessus tout, c’est transmettre ma passion, aider les jeunes à progresser et à prendre confiance en eux. L’escalade me permet de me sentir vivant, pleinement engagé. Mais ce que je ressens en partageant ces moments avec le groupe que j’encadre est tout simplement indescriptible.';}
+}
